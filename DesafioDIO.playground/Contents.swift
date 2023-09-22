411a8406a8444c7167a830123a14375204e8af28
Paulo Christiano <paulochris1611@gmail.com>
2023-09-22 12:39:09 -0300

    Resolução da atividade
    Atividade terminada

--- DesafioDIO.playground/Contents.swift
+++ DesafioDIO.playground/Contents.swift
@@ -1,4 +1,11 @@
 import UIKit
 
-var greeting = "Hello, playground"
-print("bla bla \(greeting)")
+let Nome_let: String = "Steve"
+var Nome_var: String? = "Jobs"
+
+if let nome1 = Nome_var {
+    print("Nome completo: \(Nome_let) \(nome1)")
+} else {
+    Nome_var = "Wozniak"
+    print("Nome completo: \(Nome_let) \(Nome_var!)")
+}
